cfedbc09144a7a20b6f58c8f7f30094561a517db
s18683 <61695254+s18683@users.noreply.github.com>
2020-04-08 14:03:42 +0200

    app.UseMiddleware<LoggingMiddleware>();

--- Startup.cs
+++ Startup.cs
@@ -2,21 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using Ex3V2.Middlewares;
+using Ex3V2.Models;
+using Ex3V2.Services;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Models;
-using Ex3V2.Middlewares;
-using Ex3V2.Models;
-using Ex3V2.Services;
 using static System.Net.WebRequestMethods;
+
+
 
 namespace Ex3V2
 {
@@ -57,6 +59,8 @@ namespace Ex3V2
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
+            
+            app.UseMiddleware<LoggingMiddleware>();
 
             app.Use(async (context, next) =>
             {
